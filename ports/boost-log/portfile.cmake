8021063a495f5074c2adcdc00cf155935dc35c04
Christian Ullrich <chris@chrullrich.net>
2022-02-07 14:05:49 +0100

    Build Boost with _WIN32_WINNT=_WIN32_WINNT_WIN10.
    In addition to a new pair of triplets ({x86,x64}-windows-win10) this
    requires patching Boost.Log to add an import library.
    
    See https://github.com/boostorg/log/issues/177 for details.

--- ports/boost-log/portfile.cmake
+++ ports/boost-log/portfile.cmake
@@ -6,6 +6,9 @@ vcpkg_from_github(
     REF boost-${VERSION}
     SHA512 aaae39126ae047721f71b7a55dc7a325af78c9af0b4e2d6f6d0cd7a440a0dc839acfa683c6dcb736434819e205e9d95ead56364dec60d069367d9976a45c4d17
     HEAD_REF master
+	PATCHES
+		"synchronization-lib.patch"
+		"synchronization-lib2.patch"
 )
 
 set(FEATURE_OPTIONS "")
